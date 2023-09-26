c4e055a27597212c72551e2d8be5db17fdb32d66
rawat <70885099+rawatjii@users.noreply.github.com>
2023-09-26 23:10:58 +0530

    working on single product page

--- src/views/SingleProductDetail/SingleProductDetail.jsx
+++ src/views/SingleProductDetail/SingleProductDetail.jsx
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, useLocation, useParams} from 'react-router-dom';
 import Navbar from "../../Components/Navbar/Navbar";
-import axios from "axios";
+import axios from "../../axios";
 import { useSelector } from "react-redux";
 import Footer from '../../Containers/Footer/Footer'
-// import { useParams } from "react-router-dom";
 
 const SingleProductDetail = (props)=>{
+    
+    const {productId} = useParams()
+    const [first, setfirst] = useState([])
+    // console.log('productId',productId);
+
+    const allProducts = useSelector((state)=>{
+        return state.products.allProducts;
+    })
+
+    useEffect(() => {
+        const filteredData = allProducts.filter(data=>{
+            if(data.id === productId){
+                return data;
+            }
+        })
+        console.log('filteredData',filteredData);
+    }, [])
     
 
     return(
