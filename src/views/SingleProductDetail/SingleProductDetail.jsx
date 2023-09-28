fff48eb3a5badc48bd58121a0c171bf6aeb3c08a
rawat <70885099+rawatjii@users.noreply.github.com>
2023-09-28 23:10:31 +0530

    add single product image slider

--- src/views/SingleProductDetail/SingleProductDetail.jsx
+++ src/views/SingleProductDetail/SingleProductDetail.jsx
@@ -1,44 +1,77 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { BrowserRouter as Router, Route, Switch, useLocation, useParams} from 'react-router-dom';
 import Navbar from "../../Components/Navbar/Navbar";
 import axios from "../../axios";
 import { useSelector } from "react-redux";
-import Footer from '../../Containers/Footer/Footer'
+import Footer from '../../Containers/Footer/Footer';
+import styles from './SingleProductDetail.module.css'
 
 const SingleProductDetail = (props)=>{
     
-    const {productId} = useParams()
-    const [first, setfirst] = useState([])
-    // console.log('productId',productId);
+    const {productId} = useParams();
+    const [filteredData, setFilteredData] = useState({});
+    const [productImages, setProductImages] = useState([]);
+    const [activeImage, setActiveImage] = useState();
+
+    const refs = useRef([]);
+
+    const addRefs = (el)=>{
+        if(el && !refs.current.includes(el)){
+            refs.current.push(el);
+        }
+    }
 
     const allProducts = useSelector((state)=>{
         return state.products.allProducts;
     })
 
+    const hoverHandler = (imageUrl, i)=>{
+        setActiveImage(imageUrl);
+        refs.current[i].classList.add(styles.thumbnail_active);
+        for(var j = 0; j < productImages.length; j++){
+            if(i !== j){
+                refs.current[j].classList.remove(styles.thumbnail_active);
+            }
+        }
+    }
+
     useEffect(() => {
-        const filteredData = allProducts.filter(data=>{
+        allProducts.filter(data=>{
             if(data.id === productId){
-                return data;
+                setFilteredData(data);
             }
         })
-        console.log('filteredData',filteredData);
-    }, [])
+        setProductImages(filteredData.images)
+        setActiveImage(filteredData.images?.[0]?.url)
+        console.log('data',productImages);
+    }, [allProducts, filteredData, productImages])
     
 
     return(
         <>
             <Navbar />
             <div className="totalResult">
-                <p className="">results</p>
+                <p className="">{filteredData.length} results</p>
             </div>
 
-            <div className="top_content">
+            <div className={styles.top_content}>
                 <div className="row">
-                    <div className="col-md-6 image_col">
-                        
+                    <div className="col-md-4 image_col">
+                        <div className={styles.image_carousel}>
+                            <div className={styles.thumbnails}>
+                                {productImages?.map((image, i)=>{
+                                    return <div className={i == 0 ? styles.img_wrap + ' ' + styles.thumbnail_active : styles.img_wrap} key={i} onMouseOver={()=>hoverHandler(image.url, i)} ref={addRefs}>
+                                        <img src={image.url} alt="" className="img-fluid" />
+                                    </div>
+                                })}
+                            </div>
+                            <div className="mainImage">
+                                <img src={activeImage} alt="" className="img-fluid" />
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="col-md-6 content_col">
+                    <div className="col-md-8 content_col">
 
                     </div>
                 </div>
