77b549a8f9b0f9f0ce42b9c1af3228ac386e7413
rawat <70885099+rawatjii@users.noreply.github.com>
2023-10-19 01:15:36 +0530

    add js to admin

--- src/admin/Components/Sidebar/Sidebar.jsx
+++ src/admin/Components/Sidebar/Sidebar.jsx
@@ -1,192 +1,100 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {Link, NavLink} from 'react-router-dom';
 import $ from 'jquery';	
-import { useEffect } from 'react';
+// import '../../../admin/assets/plugins/metismenu/css/metisMenu.min.css';
 
 const Sidebar = ()=>{
 
 	useEffect(()=>{
-		$('.metismenu > li > a').on('click', function(){
-			$(this).toggleClass('testing');
+		
+		// for (var i = window.location, o = $(".metismenu li a").filter(function () {
+		// 	return this.href == i;
+		// }).addclassName("").parent().addclassName("mm-active");;) {
+		// 	if (!o.is("li")) break;
+		// 	o = o.parent("").addclassName("mm-show").parent("").addclassName("mm-active");
+		// }
+
+		// metismenu
+		// $('#menu').metisMenu();
+
+		$('.metismenu > li > a').on('click',function(){
+			$(this).closest('li').find('.mm-collapse').slideToggle();
 		})
+
 	}, [])
 
     return(
         <>
-        <div class="sidebar-wrapper" data-simplebar="true">
-			<div class="sidebar-header">
+        <div className="sidebar-wrapper" data-simplebar="true">
+			<div className="sidebar-header">
 				<div>
-					<img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon" />
+					<img src="assets/images/logo-icon.png" className="logo-icon" alt="logo icon" />
 				</div>
 				<div>
-					<h4 class="logo-text">Synadmin</h4>
+					<h4 className="logo-text">Synadmin</h4>
 				</div>
-				<div class="toggle-icon ms-auto"><i class='bx bx-first-page'></i>
+				<div className="toggle-icon ms-auto"><i className='bx bx-first-page'></i>
 				</div>
 			</div>
 			{/* <!--navigation--> */}
-			<ul class="metismenu" id="menu">
+			<ul className="metismenu" id="menu">
 				<li>
-					<a href="javascript:;" class="has-arrow">
-						<div class="parent-icon"><i class='bx bx-home'></i>
+					<NavLink to='/admin'>
+						<div className="parent-icon">
+							<i className='bx bx-home'></i>	
 						</div>
-						<div class="menu-title">Dashboard</div>
+						<div className="menu-title">Dashboard</div>
+					</NavLink>
+				</li>
+
+				<li className="menu-label">Products</li>
+				<li>
+					<a href="javascript:;" className="has-arrow" aria-expanded="false">
+						<div className="parent-icon"><i className='bx bx-cart-alt' ></i>
+						</div>
+						<div className="menu-title">E-commerce</div>
 					</a>
-					<ul>
+					<ul className='mm-collapse'>
+						<li> <a href="ecommerce-products.html"><i className="bx bx-right-arrow-alt"></i>Products</a>
+						</li>
+						<li> <a href="ecommerce-products-details.html"><i className="bx bx-right-arrow-alt"></i>Product Details</a>
+						</li>
 						<li>
-							<Link to="/admin"><i class="bx bx-right-arrow-alt"></i>Dashboard</Link> 
+							<Link to="/admin/add-products"><i className="bx bx-right-arrow-alt"></i>Add New Products</Link>
+						</li>
+						<li> <a href="ecommerce-orders.html"><i className="bx bx-right-arrow-alt"></i>Orders</a>
 						</li>
 					</ul>
 				</li>
-				<li class="menu-label">UI Elements</li>
+				<li className="menu-label">Pages</li>
 				<li>
-					<a href="javascript:;" class="has-arrow">
-						<div class="parent-icon"><i class='bx bx-cart-alt' ></i>
+					<a className="has-arrow" href="javascript:;" aria-expanded="false">
+						<div className="parent-icon"><i className='bx bx-lock-open-alt'></i>
 						</div>
-						<div class="menu-title">eCommerce</div>
+						<div className="menu-title">Authentication</div>
 					</a>
-					<ul>
-						<li> <a href="ecommerce-products.html"><i class="bx bx-right-arrow-alt"></i>Products</a>
+					<ul className='mm-collapse'>
+						<li> <a href="authentication-signin.html" target="_blank"><i className="bx bx-right-arrow-alt"></i>Sign In</a>
 						</li>
-						<li> <a href="ecommerce-products-details.html"><i class="bx bx-right-arrow-alt"></i>Product Details</a>
+						<li> <a href="authentication-signup.html" target="_blank"><i className="bx bx-right-arrow-alt"></i>Sign Up</a>
 						</li>
-						<li>
-							<Link to="/admin/add-products"><i class="bx bx-right-arrow-alt"></i>Add New Products</Link>
+						<li> <a href="authentication-signin-with-header-footer.html" target="_blank"><i className="bx bx-right-arrow-alt"></i>Sign In with Header & Footer</a>
 						</li>
-						<li> <a href="ecommerce-orders.html"><i class="bx bx-right-arrow-alt"></i>Orders</a>
+						<li> <a href="authentication-signup-with-header-footer.html" target="_blank"><i className="bx bx-right-arrow-alt"></i>Sign Up with Header & Footer</a>
 						</li>
-					</ul>
-				</li>
-				<li class="menu-label">Pages</li>
-				<li>
-					<a class="has-arrow" href="javascript:;">
-						<div class="parent-icon"><i class='bx bx-lock-open-alt'></i>
-						</div>
-						<div class="menu-title">Authentication</div>
-					</a>
-					<ul>
-						<li> <a href="authentication-signin.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>Sign In</a>
+						<li> <a href="authentication-forgot-password.html" target="_blank"><i className="bx bx-right-arrow-alt"></i>Forgot Password</a>
 						</li>
-						<li> <a href="authentication-signup.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>Sign Up</a>
+						<li> <a href="authentication-reset-password.html" target="_blank"><i className="bx bx-right-arrow-alt"></i>Reset Password</a>
 						</li>
-						<li> <a href="authentication-signin-with-header-footer.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>Sign In with Header & Footer</a>
-						</li>
-						<li> <a href="authentication-signup-with-header-footer.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>Sign Up with Header & Footer</a>
-						</li>
-						<li> <a href="authentication-forgot-password.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>Forgot Password</a>
-						</li>
-						<li> <a href="authentication-reset-password.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>Reset Password</a>
-						</li>
-						<li> <a href="authentication-lock-screen.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>Lock Screen</a>
+						<li> <a href="authentication-lock-screen.html" target="_blank"><i className="bx bx-right-arrow-alt"></i>Lock Screen</a>
 						</li>
 					</ul>
 				</li>
 				<li>
 					<a href="user-profile.html">
-						<div class="parent-icon"><i class='bx bx-user-pin' ></i>
+						<div className="parent-icon"><i className='bx bx-user-pin' ></i>
 						</div>
-						<div class="menu-title">User Profile</div>
-					</a>
-				</li>
-				<li>
-					<a href="timeline.html">
-						<div class="parent-icon"> <i class="bx bx-video-recording"></i>
-						</div>
-						<div class="menu-title">Timeline</div>
-					</a>
-				</li>
-				<li>
-					<a class="has-arrow" href="javascript:;">
-						<div class="parent-icon"><i class="bx bx-error"></i>
-						</div>
-						<div class="menu-title">Errors</div>
-					</a>
-					<ul>
-						<li> <a href="errors-404-error.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>404 Error</a>
-						</li>
-						<li> <a href="errors-500-error.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>500 Error</a>
-						</li>
-						<li> <a href="errors-coming-soon.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>Coming Soon</a>
-						</li>
-						<li> <a href="error-blank-page.html" target="_blank"><i class="bx bx-right-arrow-alt"></i>Blank Page</a>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<a href="faq.html">
-						<div class="parent-icon"><i class="bx bx-help-circle"></i>
-						</div>
-						<div class="menu-title">FAQ</div>
-					</a>
-				</li>
-				<li>
-					<a href="pricing-table.html">
-						<div class="parent-icon"><i class='bx bx-dollar-circle'></i>
-						</div>
-						<div class="menu-title">Pricing</div>
-					</a>
-				</li>
-				<li class="menu-label">Charts & Maps</li>
-				<li>
-					<a class="has-arrow" href="javascript:;">
-						<div class="parent-icon"><i class="bx bx-line-chart"></i>
-						</div>
-						<div class="menu-title">Charts</div>
-					</a>
-					<ul>
-						<li> <a href="charts-apex-chart.html"><i class="bx bx-right-arrow-alt"></i>Apex</a>
-						</li>
-						<li> <a href="charts-chartjs.html"><i class="bx bx-right-arrow-alt"></i>Chartjs</a>
-						</li>
-						<li> <a href="charts-highcharts.html"><i class="bx bx-right-arrow-alt"></i>Highcharts</a>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<a class="has-arrow" href="javascript:;">
-						<div class="parent-icon"><i class='bx bx-map-pin' ></i>
-						</div>
-						<div class="menu-title">Maps</div>
-					</a>
-					<ul>
-						<li> <a href="map-google-maps.html"><i class="bx bx-right-arrow-alt"></i>Google Maps</a>
-						</li>
-						<li> <a href="map-vector-maps.html"><i class="bx bx-right-arrow-alt"></i>Vector Maps</a>
-						</li>
-					</ul>
-				</li>
-				<li class="menu-label">Others</li>
-				<li>
-					<a class="has-arrow" href="javascript:;">
-						<div class="parent-icon"><i class="bx bx-menu"></i>
-						</div>
-						<div class="menu-title">Menu Levels</div>
-					</a>
-					<ul>
-						<li> <a class="has-arrow" href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Level One</a>
-							<ul>
-								<li> <a class="has-arrow" href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Level Two</a>
-									<ul>
-										<li> <a href="javascript:;"><i class="bx bx-right-arrow-alt"></i>Level Three</a>
-										</li>
-									</ul>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<a href="https://codervent.com/synadmin/documentation/index.html" target="_blank">
-						<div class="parent-icon"><i class='bx bx-file' ></i>
-						</div>
-						<div class="menu-title">Documentation</div>
-					</a>
-				</li>
-				<li>
-					<a href="https://themeforest.net/user/codervent" target="_blank">
-						<div class="parent-icon"><i class='bx bx-headphone' ></i>
-						</div>
-						<div class="menu-title">Support</div>
+						<div className="menu-title">User Profile</div>
 					</a>
 				</li>
 			</ul>
